b16f45596f43a1e8819268cc075d3e84e237e658
AdnCodez <adnan@adncodez.com>
2020-10-11 17:53:39 +0100

    added main pages for this project

---
+++ gatsby/src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function FourOhFourPage() {
+  return (
+    <div>
+      <p>this is a 404 page</p>
+    </div>
+  );
+}
---
+++ gatsby/src/pages/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function HomePage() {
+  return <p>hello this is a p tag</p>;
+}

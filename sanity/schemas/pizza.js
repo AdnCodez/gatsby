f472a51396e046f9f1567cfdf540e913e8f615e9
AdnCodez <adnan@adncodez.com>
2020-10-12 18:34:43 +0100

    setup of headless CMS, creation of content types, custom previews, data relationships, data type, custom CMS inputs

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -1,4 +1,5 @@
 import { MdLocalPizza as icon } from 'react-icons/md';
+import PriceInput from '../components/PriceInput';
 
 export default {
   // computer name
@@ -37,7 +38,42 @@ export default {
       type: 'number',
       description: 'Price of the pizza in cents',
       validation: (Rule) => Rule.min(1000),
-      // TODO: add custom input component
+      // DONE: add custom input component
+      inputComponent: PriceInput,
+    },
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [{ type: 'topping' }],
+        },
+      ],
     },
   ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      // filter undefined toppings out
+      // const tops = Object.values(toppings).filter(
+      //   (topping) => topping !== undefined
+      // );
+      const tops = Object.values(toppings).filter(Boolean);
+      // return the preview object for the pizza
+      return {
+        title,
+        media,
+        subtitle: tops.join(', '),
+      };
+    },
+  },
 };
